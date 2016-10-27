003490b291190450d2100541d5768be4db8fae35
Vlad Markin <vlad.d.markin@gmail.com>
2016-10-27 10:06:00 -0400

    Created menu component

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+import Menu from './menu.jsx';
+
 
 class App extends React.Component {
   render() {
-    return (<p>Hello!</p>);
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}><h1>Effective travel <small>developer tools</small></h1></Col>
+        </Row>
+        
+        <Row>
+          <Col md={2}>
+            <Menu/>
+          </Col>
+          
+          <Col md={10}>
+            Controls are here
+          </Col>
+        </Row>
+      </Grid>
+      );
   }
 }
 
