e710e4a517a64f51d8e37c47325fed4bec4899ba
Vlad Markin <vlad.d.markin@gmail.com>
2016-11-01 09:25:42 -0400

    Able to add stations now

--- app/stations.jsx
+++ app/stations.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Button, FormGroup, FormControl, Table, ListGroup, ListGroupItem} from 'react-bootstrap';
+import { Button, ControlLabel, FormGroup, FormControl, Table, ListGroup, ListGroupItem, Alert, ButtonToolbar } from 'react-bootstrap';
+
+
+class StationsAlert extends React.Component {
+  render() {
+    return(<Alert >{this.props.text}</Alert>)
+  }
+}
 
 
 export default class Stations extends React.Component {
@@ -11,7 +18,13 @@ export default class Stations extends React.Component {
       stationsList : [],
       newStation : {
         title : ''
-      }
+      },
+      
+      submitDisabled : false,
+      
+      showAlert: false,
+      alertStyle : 'warning',
+      alertText : ''
     };
   }
   
@@ -27,55 +40,80 @@ export default class Stations extends React.Component {
     }.bind(this));
   }
   
+  submitNewStation() {
+    console.log('New station ' + this.state.newStation.title);
+    this.setState({ submitDisabled : true });
+    
+    this.state.stations.post(this.state.newStation).then(function(resp){
+      if(resp.success) {
+        this.update();
+        this.setState({ showAlert: true, alertStyle : 'success', alertText : 'Successfully added new station' });
+      }
+      else {
+        this.setState({ showAlert: true, alertStyle : 'danger', alertText : 'Failed to add new station. Error: ' + JSON.stringify(resp.error) });
+        console.log(resp.error);
+      }
+      this.setState({ submitDisabled : false });
+    }.bind(this));
+  }
+  
+  handleTitleChange(e) {
+    this.setState({ newStation : { title : e.target.value }} );
+  }
+  
   render() {
     this.update();
     
     var stationTableItems = this.state.stationsList.map(function(station, idx){
       return(
         <tr key={idx}>
-          <td>{idx}</td>
+          <td>{idx + 1}</td>
           <td>{station.title}</td>
           <td>{station.s_id}</td>
           <td>not yet</td>
+          <td>
+            <ButtonToolbar>
+              <Button bsStyle="primary" bsSize="xsmall">Edit</Button>
+              <Button bsStyle="danger" bsSize="xsmall">Remove</Button>
+            </ButtonToolbar>
+          </td>
         </tr>
       );
     });
     
+    var alert = this.state.showAlert ? <Alert bsStyle={this.state.alertStyle}>{this.state.alertText}</Alert> : '';
+    
     return (
-       <Table striped bordered condensed hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>S_ID</th>
-              <th>Coordinates</th>
-            </tr>
-          </thead>
-          <tbody>
-            {stationTableItems}
-          </tbody>
-       </Table>
-      
-      <form>
-        <FormGroup controlId="newStationTitle">
-          <ControlLabel>Title: </ControlLabel>
-          <FormControl
-            type="text"
-            value={this.newStation.title};
-            placeholder="Enter new station title"
-            onChange={this.handleTitleChange}></FormControl>
-        </FormGroup>
-        
+      <div>
+        {alert}
+         <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>S_ID</th>
+                <th>Coordinates</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stationTableItems}
+            </tbody>
+         </Table>
 
-      </form>
+        <form>
+          <FormGroup controlId="newStationTitle">
+            <h3>Add new station</h3>
+            <ControlLabel>Title: </ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.newStation.title}
+              placeholder="Enter new station title"
+              onChange={this.handleTitleChange.bind(this)}></FormControl>
+          </FormGroup>
+          <Button onClick={this.submitNewStation.bind(this)} disabled={this.state.submitDisabled} >Submit</Button>          
+        </form>
+     </div>
     );
-  }
-  
-  submitNewStation() {
-    console.log('New station ' + this.state.newStation);
-  }
-  
-  handleTitleChange(e) {
-    this.setState({ newStation.title : e.target.value });
   }
 }
